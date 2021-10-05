735a199c144579c7714f6f92e6c07a93444ccb8c
donellgill <donell.gill@outlook.com>
2021-10-05 13:58:27 -0400

    created user and profile tables

---
+++ db/migrate/20211005174425_create_profiles.rb
@@ -0,0 +1,11 @@
+class CreateProfiles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :profiles do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
