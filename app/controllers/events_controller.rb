3244311cf53932e717348b447116f4a2c374d4f3
donellgill <donell.gill@outlook.com>
2021-10-07 09:01:19 -0400

    renamed event columns for logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,34 @@
 class EventsController < ApplicationController
+    def index
+        events = Event.all
+        render json: events 
+    end
+
+    def create
+        event = Event.create(event_params) 
+        render json: event, status: :created
+    end
+
+    def show 
+        event = Event.find(params[:id])
+        render json: event
+    end
+
+    def update
+        event = Event.find_by(id: params[:id])
+        event.update(event_params)
+        render json: event
+    end
+
+    def destroy
+        event = Event.find(params[:id])
+        event.destroy
+        head :no_content
+    end
+
+    private
+
+    def event_params
+        params.permit(:event_name, :posting_date, :location, :desc)
+    end
 end
