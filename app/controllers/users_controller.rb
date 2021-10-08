c958932e201d4cf7d824b34acd5d878e62638bf9
donellgill <donell.gill@outlook.com>
2021-10-08 11:46:59 -0400

    added sessions controller and route config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     end   
 
     def show
-        user = User.find(params[:id])
+        user = User.find_by(params[:id])
         render json: user
     end
 
