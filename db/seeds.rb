9d3bb795ffe7f4e5754179c6d6d0cb82a1324182
donellgill <donell.gill@outlook.com>
2021-10-13 17:21:22 -0400

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,28 @@ Category.destroy_all
 Activity.destroy_all
 Event.destroy_all
 
-User.create!(firstname: 'Abby', lastname: 'Allus', username: 'Abster33')
-User.create!(firstname: 'Betty', lastname: 'Black', username: 'Bettyliscious')
-User.create!(firstname: 'Common', lastname: 'Carson', username: 'Common')
-User.create!(firstname: 'Delta', lastname: 'Delphina', username: 'Delta')
+user1 = User.create!(firstname: 'Abby', lastname: 'Allus', username: 'Abster33')
+user2 = User.create!(firstname: 'Betty', lastname: 'Black', username: 'Bettyliscious')
+user3 = User.create!(firstname: 'Common', lastname: 'Carson', username: 'Common')
+user4 = User.create!(firstname: 'Delta', lastname: 'Delphina', username: 'Delta')
 
-Profile.create!(username: 'Abster33', email: 'Abster33@gmail.com')
-Profile.create!(username: 'Bettyliscious', email: 'Blackbetty@123.com')
-Profile.create!(username: 'Common', email: 'alltoocommon@hotmail.com')
-Profile.create!(username: 'Delta', email: 'doubledee@aol.com')
+profile1 = Profile.create!(username: 'Abster33', email: 'Abster33@gmail.com')
+profile2 = Profile.create!(username: 'Bettyliscious', email: 'Blackbetty@123.com')
+profile3 = Profile.create!(username: 'Common', email: 'alltoocommon@hotmail.com')
+profile4 = Profile.create!(username: 'Delta', email: 'doubledee@aol.com')
 
-Category.create!(category_name: 'Sports')
-Category.create!(category_name: 'Arts & Crafts')
-Category.create!(category_name: 'Concerts')
-Category.create!(category_name: 'Group Dance')
+category1 = Category.create!(category_name: 'Sports')
+category2 = Category.create!(category_name: 'Arts & Crafts')
+category3 = Category.create!(category_name: 'Concerts')
+category4 = Category.create!(category_name: 'Group Dance')
 
-Activity.create!(activity_name: 'Basketball', category_id: Category.first.id)
-Activity.create!(activity_name: 'Crochet Class', category_id: Category.second.id)
-Activity.create!(activity_name: 'Foo Fighters World Tour', category_id: Category.third.id)
-Activity.create!(activity_name: 'Learn to dance salsa', category_id: Category.fourth.id)
+activity1 = Activity.create!(activity_name: 'Basketball', category_id: Category.first.id)
+activity2 = Activity.create!(activity_name: 'Crochet Class', category_id: Category.second.id)
+activity3 = Activity.create!(activity_name: 'Foo Fighters World Tour', category_id: Category.third.id)
+activity4 = Activity.create!(activity_name: 'Learn to dance salsa', category_id: Category.fourth.id)
 
-Event.create!(event_name: 'Orange Park Basketball', posting_date: 10-31-2021, desc: "Fun times")
-Event.create!(event_name: 'Crochet Class at The Winehouse', posting_date: 10-31-2021, desc: "Come join us!")
-Event.create!(event_name: 'Foo Fighters at Prudential Center', posting_date: 10-31-2021, desc: "See you there!")
-Event.create!(event_name: 'Salsa by Vicki', posting_date: 10-31-2021, desc: "Dance with us!")
+event1 = Event.create!(event_name: 'Orange Park Basketball', posting_date: 10-31-2021, desc: "Fun times")
+event2 = Event.create!(event_name: 'Crochet Class at The Winehouse', posting_date: 10-31-2021, desc: "Come join us!")
+event3 = Event.create!(event_name: 'Foo Fighters at Prudential Center', posting_date: 10-31-2021, desc: "See you there!")
+event4 = Event.create!(event_name: 'Salsa by Vicki', posting_date: 10-31-2021, desc: "Dance with us!")
 
