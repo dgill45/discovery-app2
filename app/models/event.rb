347f62561ec5347ae3089beed3c830bf5f755b57
donellgill <donell.gill@outlook.com>
2021-10-15 19:08:04 -0400

    added validations associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :activity
+    validates :desc, length: {max: 75}
     has_many :users, through: :users_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
     has_one :profile 
     has_many :events, through: :users_events
 end
